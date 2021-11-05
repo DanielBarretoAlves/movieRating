56bc9cad001ada334b48f519e0e91b1a6d0f5e14
Daniel Barreto Alves <danielbarreto559@gmail.com>
2021-11-05 10:26:37 -0300

    Menu Method

--- Program.cs
+++ Program.cs
@@ -2,10 +2,38 @@
 using System.Collections.Generic;
 using System.Linq;
 
-namespace MyApp // Note: actual namespace depends on the project name.
+namespace MyApp
 {
+
     public class Program
     {
+
+        public static int menu(string[] title)
+        {
+            bool key = false;
+            int escolha = 0;
+            while (key != true)
+            {
+                Console.WriteLine("Select Movie ID:");
+                escolha = int.Parse(Console.ReadLine());
+                Console.WriteLine("Is this the Movie? " + title[escolha]);
+                Console.WriteLine("1 - Yes");
+                Console.WriteLine("2 - No");
+                int choice = int.Parse(Console.ReadLine());
+                if (choice == 1)
+                {
+                    key = true;
+                }
+            }
+
+
+            
+            return escolha;
+        }
+
+
+
+
         public static void Main(string[] args)
         {
             Bulkloader bulk = new Bulkloader();
@@ -15,21 +43,20 @@ namespace MyApp // Note: actual namespace depends on the project name.
 
             // Get Data From CSV Files
             Console.WriteLine("Loading Files..");
-            int[] rMovieID = bulk.readFileInt(path1,1);
-            float[] rating = bulk.readFileFloat(path1,2);
-            int[] movieId = bulk.readFileInt(path2,0);
-            int[] mlsec = bulk.readFileInt(path1,3);
-            string[] title = bulk.readFileString(path2,1);
+            int[] rMovieID = bulk.readFileInt(path1, 1);
+            float[] rating = bulk.readFileFloat(path1, 2);
+            int[] movieId = bulk.readFileInt(path2, 0);
+            int[] mlsec = bulk.readFileInt(path1, 3);
+            string[] title = bulk.readFileString(path2, 1);
             Console.WriteLine("Load Files Complete!");
-            // string[] genres = bulk.readFileString(path2,2);
 
 
             Console.WriteLine("Sorting Data..");
 
-            // movieID = bulk.bubble(movieID);
+
             //bubble sort -------------------------------------------------------
             int temp = 0;
-            int tempSec =0;
+            int tempSec = 0;
             float tempRating = 0;
 
             for (int i = 0; i < rMovieID.Length; i++)
@@ -38,7 +65,7 @@ namespace MyApp // Note: actual namespace depends on the project name.
                 {
                     if (rMovieID[j] > rMovieID[j + 1])
                     {
-                        
+
                         temp = rMovieID[j + 1];
                         rMovieID[j + 1] = rMovieID[j];
                         rMovieID[j] = temp;
@@ -57,13 +84,12 @@ namespace MyApp // Note: actual namespace depends on the project name.
             Console.WriteLine("Data Sorted Successfully!");
 
 
-            Console.WriteLine("Select Movie ID:");
-            int escolha = int.Parse(Console.ReadLine());
-            Console.WriteLine("Movie: "+title[escolha]);
-            float notas = 0;
-            int qtd =0;
-            
+
+            int escolha = menu(title);
+
             //Adaptar para Rodar em Array
+            float notas = 0;
+            int qtd = 0;
             if (escolha != 0)
             {
                 escolha--;
@@ -77,22 +103,20 @@ namespace MyApp // Note: actual namespace depends on the project name.
                 if (rMovieID[i] == movieId[test])
                 {
                     escolha = test;
-                    notas+=rating[i];
+                    notas += rating[i];
                     qtd++;
-                    
+
                 }
-                
+
             }
-            Console.WriteLine(qtd+" Review Found");
+            Console.WriteLine(qtd + " Review Found");
             Console.WriteLine("===========================================");
-            Console.WriteLine("Avarage Review for the movie:  - "+notas/qtd);
+            Console.WriteLine("Avarage Review for the movie:  - " + notas / qtd);
             Console.WriteLine("===========================================");
 
 
-            
+
         }
     }
 }
 
-
-//1,1,4.0,964982703
